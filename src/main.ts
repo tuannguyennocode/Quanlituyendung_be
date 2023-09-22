1be3b9226eee8ecae8a46959ba5cd809b40bc41a
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-09-22 23:25:41 +0700

    enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Thay đổi thành tên miền hoặc URL của máy chủ gốc của bạn
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Cho phép truy cập có danh tính (đối với các cookie hoặc chứng thực)
+  });
   await app.listen(5000);
 }
 bootstrap();
