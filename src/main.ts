d56265b7c1b962ac05252d7670397c75417bc8ec
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-09-23 23:55:11 +0700

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
-import { createDocument } from 'src/swagger/swagger';
+import { createDocument } from './swagger/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
