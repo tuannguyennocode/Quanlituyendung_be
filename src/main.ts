86d4724061de5dbf4050891a3df2a0be3d4c30da
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-09-24 00:16:50 +0700

    fix swagger

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,44 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule,DocumentBuilder  } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { createDocument } from './swagger/swagger';
+import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.setGlobalPrefix('/api');
+  // SwaggerModule.setup('api', app, createDocument(app), {
+  //   swaggerOptions: {
+  //     persistAuthorization: true,
+  //   },
+  // });
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
-  .setVersion('1.0')
-  .addTag('cats')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-  // const config = new DocumentBuilder()
-  //   .setTitle('Quan ly tuyen dung')
-  //   .setDescription('Quan ly tuyen dung API description')
-  //   .setVersion('1.0')
-  //   .addTag('Quan ly tuyen dung')
-  //   .build();
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+    .setTitle('Quan ly tuyen dung')
+    .setDescription('Quan ly tuyen dung API description')
+    .setVersion('1.0')
+    .addTag('Quan ly tuyen dung')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.enableCors({
     origin: 'http://localhost:3000', // Thay đổi thành tên miền hoặc URL của máy chủ gốc của bạn
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Cho phép truy cập có danh tính (đối với các cookie hoặc chứng thực)
   });
   await app.listen(5000);
+  if (process.env.NODE_ENV === 'development') {
+    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
+
+    // write swagger json file
+    const pathToSwaggerJson = resolve(
+      pathToSwaggerStaticFolder,
+      'swagger.json',
+    );
+    const swaggerJson = JSON.stringify(document, null, 2);
+    writeFileSync(pathToSwaggerJson, swaggerJson);
+    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
+  }
 }
 bootstrap();
