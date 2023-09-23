ad3d90a72d773fff235024b052474d0d86b4cfe5
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-09-23 23:40:16 +0700

    fix main

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule } from '@nestjs/swagger';
-import { createDocument } from 'swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api');
-  SwaggerModule.setup('api', app, createDocument(app), {
-    swaggerOptions: {
-      persistAuthorization: true,
-    }
-  });
+
   // const config = new DocumentBuilder()
   //   .setTitle('Quan ly tuyen dung')
   //   .setDescription('Quan ly tuyen dung API description')
