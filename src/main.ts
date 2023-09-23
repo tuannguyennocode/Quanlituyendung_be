b420b4562ef82b4b6c198f70981416d2028d1ce6
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-09-23 22:40:45 +0700

    test

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 /* eslint-disable prettier/prettier */
-
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule } from '@nestjs/swagger';
 import { createDocument } from 'swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+  app.setGlobalPrefix('/api');
+  SwaggerModule.setup('api', app, createDocument(app), {
+    swaggerOptions: {
+      persistAuthorization: true,
+    }
+  });
+  // const config = new DocumentBuilder()
+  //   .setTitle('Quan ly tuyen dung')
+  //   .setDescription('Quan ly tuyen dung API description')
+  //   .setVersion('1.0')
+  //   .addTag('Quan ly tuyen dung')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
   app.enableCors({
     origin: 'http://localhost:3000', // Thay đổi thành tên miền hoặc URL của máy chủ gốc của bạn
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
