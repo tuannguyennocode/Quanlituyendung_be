84757b8a29f9b36a38d264df7d09a81cfea603b0
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-09-24 00:24:49 +0700

    fix swagger ui

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserAccountModule } from './user-account/user-account.module';
 import { AuthModule } from './auth/auth.module';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'swagger-static'),
-      serveRoot: process.env.NODE_ENV === 'development' ? '/' : '/swagger',
-    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
-    MongooseModule.forRoot(process.env.DB_URI, {
-      dbName: 'quanlituyendung_db',
-    }),
+    MongooseModule.forRoot(process.env.DB_URI, { dbName: 'quanlituyendung_db' }),
     UserAccountModule,
     AuthModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,38 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 import { createDocument } from './swagger/swagger';
-import { resolve } from 'path';
-import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('/api');
-  // SwaggerModule.setup('api', app, createDocument(app), {
-  //   swaggerOptions: {
-  //     persistAuthorization: true,
-  //   },
-  // });
-  const config = new DocumentBuilder()
-    .setTitle('Quan ly tuyen dung')
-    .setDescription('Quan ly tuyen dung API description')
-    .setVersion('1.0')
-    .addTag('Quan ly tuyen dung')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  app.setGlobalPrefix('/api');
+  SwaggerModule.setup('api', app, createDocument(app), {
+    customSiteTitle: 'Backend Generator',
+    customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  });
+  // const config = new DocumentBuilder()
+  //   .setTitle('Quan ly tuyen dung')
+  //   .setDescription('Quan ly tuyen dung API description')
+  //   .setVersion('1.0')
+  //   .addTag('Quan ly tuyen dung')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, document);
   app.enableCors({
     origin: 'http://localhost:3000', // Thay đổi thành tên miền hoặc URL của máy chủ gốc của bạn
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Cho phép truy cập có danh tính (đối với các cookie hoặc chứng thực)
   });
   await app.listen(5000);
-  if (process.env.NODE_ENV === 'development') {
-    const pathToSwaggerStaticFolder = resolve(process.cwd(), 'swagger-static');
-
-    // write swagger json file
-    const pathToSwaggerJson = resolve(
-      pathToSwaggerStaticFolder,
-      'swagger.json',
-    );
-    const swaggerJson = JSON.stringify(document, null, 2);
-    writeFileSync(pathToSwaggerJson, swaggerJson);
-    console.log(`Swagger JSON file written to: '/swagger-static/swagger.json'`);
-  }
 }
 bootstrap();
