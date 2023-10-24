185d0ac4809ed3b7d2062814ee5cc236217025dc
ThanhDa <thanhda10cl@gmail.com>
2023-10-24 22:03:24 +0700

    update controller

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
-
+import { Get, Put, Delete, Param, Body, Post, Request } from '@nestjs/common';
+import { Controller } from '@nestjs/common/decorators';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/auth/auth.public.decorator';
+import { CreateCompanyForm } from './form/createcompanyform';
+import { CompanyService } from './company.service';
+import { UpdateCompanyForm } from './form/updatecompanyform';
+@ApiTags('Company')
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+    constructor(private readonly companyService: CompanyService) {}
+    @ApiBearerAuth()
+    @Post()
+    createCompany(@Body() createCompanyForm: CreateCompanyForm, @Request() req) {
+        return this.companyService.createCompany(createCompanyForm, req);
+    }
+    @ApiBearerAuth()
+    @Public()
+    @Get(':id')
+    getCompany(@Param('id') id: string) {
+        return this.companyService.getCompanyById(id);
+    }
+    @ApiBearerAuth()
+    @Public()
+    @Get()
+    getAllCompany() {
+        return this.companyService.getAllCompany();
+    }
+
+    @ApiBearerAuth()
+    @Put()
+    updateCompany(@Body() updateCompanyForm: UpdateCompanyForm) {
+        return this.companyService.updateCompany(updateCompanyForm);
+    }
+
+    @ApiBearerAuth()
+    @Delete(':id')
+    deleteCompany(@Param('id') id: string) {
+        return this.companyService.deleteCompany(id);
+    }
+}
