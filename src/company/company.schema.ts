08b349aa642b415721222c4cd7e991cdf8400082
Tuan Nguyen <tuanbmt123123@gmail.com>
2023-10-28 19:21:02 +0700

    fix import common

--- src/company/company.schema.ts
+++ src/company/company.schema.ts
@@ -1,33 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { CommonSchemaProps } from 'src/common/commonSchemaProps';
+import { CommonSchemaProps } from '../common/commonSchemaProps';
 import { JobPosting } from 'src/jobposting/jobposting.schema';
 
 export type CompanyDocument = HydratedDocument<Company>;
 
 @Schema()
-export class Company extends CommonSchemaProps{
-  _id: mongoose.Types.ObjectId;
-  @Prop()
-  name: string;
-  
-  @Prop({unique:true})
-  phoneNumber: string;
+export class Company extends CommonSchemaProps {
+    _id: mongoose.Types.ObjectId;
+    @Prop()
+    name: string;
 
-  @Prop({unique:true})
-  email: string;
+    @Prop({ unique: true })
+    phoneNumber: string;
 
-  @Prop()
-  address: string;
+    @Prop({ unique: true })
+    email: string;
 
-  @Prop()
-  company_size: string;
+    @Prop()
+    address: string;
 
-  @Prop()
-  web_url: string;
+    @Prop()
+    company_size: string;
 
-  @Prop()
-  description: string;
+    @Prop()
+    web_url: string;
+
+    @Prop()
+    description: string;
 }
 
 export const CompanySchema = SchemaFactory.createForClass(Company);
--- src/jobposting/jobposting.schema.ts
+++ src/jobposting/jobposting.schema.ts
@@ -1,28 +1,28 @@
 /* eslint-disable prettier/prettier */
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
-import { CommonSchemaProps } from 'src/common/commonSchemaProps';
+import { CommonSchemaProps } from '../common/commonSchemaProps';
 
 export type JobPostingDocument = HydratedDocument<JobPosting>;
 
 @Schema()
-export class JobPosting extends CommonSchemaProps{
-  _id: mongoose.Types.ObjectId;
+export class JobPosting extends CommonSchemaProps {
+    _id: mongoose.Types.ObjectId;
 
-  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Company' })
-  company: mongoose.Types.ObjectId;
-  
-  @Prop()
-  name: string;
-  // Thêm trường startDate và endDate
-  @Prop()
-  startDate: string; // Định dạng ngày bắt đầu
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Company' })
+    company: mongoose.Types.ObjectId;
 
-  @Prop()
-  endDate: string; // Định dạng ngày kết thúc
-  // Thêm trường detail theo cấu trúc bạn đã mô tả
-  @Prop({ type: JSON })
-  detail: object;
+    @Prop()
+    name: string;
+    // Thêm trường startDate và endDate
+    @Prop()
+    startDate: string; // Định dạng ngày bắt đầu
+
+    @Prop()
+    endDate: string; // Định dạng ngày kết thúc
+    // Thêm trường detail theo cấu trúc bạn đã mô tả
+    @Prop({ type: JSON })
+    detail: object;
 }
 
 export const JobPostingSchema = SchemaFactory.createForClass(JobPosting);
